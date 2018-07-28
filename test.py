8d083e48cae5d518fcd84ad302c0774441a2994f
Anxcity <1536739330@qq.com>
2018-07-28 16:05:28 +0800

    tset

---
+++ test.py
@@ -0,0 +1,14 @@
+"""class student(object):
+    _slots_ = ('name','age')
+
+s = student()
+s.name = 'Mechael'
+s.age = 25
+print(s.age)"""
+
+from bs4 import BeautifulSoup
+import requests
+import re
+
+
+
